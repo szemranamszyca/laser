cbca904ddbe0f64849bd0613165ee16a80374455
Mszyca Stefan <szemranamszyca@yandex.ru>
2018-11-18 20:55:50 +0100

    add gtest

---
+++ test/testControlPanel/TestControlPanel.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "TestControlPanel.hpp"
+
+using namespace std;
+
+TestControlPanel::TestControlPanel() {}
+
+TestControlPanel::~TestControlPanel() {}
+
+void TestControlPanel::SetUp() {}
+
+void TestControlPanel::TearDown() {}
+
+TEST(getNameTest, ShouldReturnSampleLib1Name) {
+    ASSERT_TRUE(false);
+}
